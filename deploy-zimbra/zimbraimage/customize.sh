9f5783a86454ffbc66a2d15725c35200035907f0
Yeak Nai Siew <yeak@mailhappen.com>
2025-01-06 11:28:56 +0800

    More checking

--- deploy-zimbra/zimbraimage/customize.sh
+++ deploy-zimbra/zimbraimage/customize.sh
@@ -31,9 +31,12 @@ function install_logo () {
   chmod 755 /opt/zimbra/jetty/webapps/zimbra/logos
 
   cd /opt/zimbra/jetty/webapps/zimbra/logos
-  curl -L --max-time 30 $banner -o logo.svg
+  curl --max-time 30 -L $banner -o logo.svg
   RS=$?
-  [ $RS -ne 0 ] && /usr/bin/cp -f /root/mailhappen-docker.svg logo.svg
+  if [ $RS -ne 0 ]; then
+    rm -f logo.svg
+    return 1
+  fi
   chmod 644 logo.svg
 
   tmp="/tmp/logo.$$"
@@ -50,10 +53,34 @@ logobanner="${LOGO_BANNER:=http://minio.mailhappen.com/downloads/mailhappen-dock
 logourl="${LOGO_URL:=https://github.com/Mailhappen/}"
 install_logo $logobanner $logourl
 
-# 3. zmstat-cleanup
-function cleanup_zmstat() {
-  keep=$1
-  [ -z "$keep" ] && keep=60
-  /opt/zimbra/libexec/zmstat-cleanup -k $keep
+# 3. Deploy maldua 2fa
+function deploy_maldua_2fa() {
+  cd /tmp
+  curl --max-time 30 -LO https://github.com/maldua-suite/zimbra-ose-2fa/releases/download/v0.8.0/zimbra-ose-2fa_0.8.0.tar.gz
+  RS=$?
+  if [ $RS -ne 0 ]; then
+    rm -f zimbra-ose-2fa_0.8.0.tar.gz
+    return 1
+  fi
+  tar xf zimbra-ose-2fa_0.8.0.tar.gz
+  cd zimbra-ose-2fa_0.8.0
+  ./install.sh
+  su - zimbra -c 'zmmailboxdctl restart'
 }
-cleanup_zmstat 30
+deploy_maldua_2fa
+
+# 4. zmstat-cleanup
+function zmstat_cleanup_crontab() {
+  crontab -u zimbra -l > /tmp/cron.zimbra
+  grep -q zmstat-cleanup /tmp/cron.zimbra
+  RS=$?
+  [ $RS -eq 0 ] && return 0
+  cat >> /tmp/cron.zimbra <<EOT
+#
+# zmstat_cleanup
+#
+15 0 * * 7 /opt/zimbra/libexec/zmstat-cleanup -k 30
+EOT
+  crontab -u zimbra /tmp/cron.zimbra
+}
+zmstat_cleanup_crontab
