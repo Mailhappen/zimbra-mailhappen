4aab03708bfcfc3d2e0df0399d0a78b12250f16a
Yeak Nai Siew <yeak@mailhappen.com>
2025-01-06 17:55:09 +0800

    Stay with latest tag.

--- deploy-zimbra/zimbraimage/build.sh
+++ deploy-zimbra/zimbraimage/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker build -t yeak/zimbraimage:10.1.4 .
+docker build -t yeak/zimbraimage .
 
 # archive - still needed for upgrade testing
 docker build -t yeak/zimbraimage:10.1.0 . -f Dockerfile-10.1.0
