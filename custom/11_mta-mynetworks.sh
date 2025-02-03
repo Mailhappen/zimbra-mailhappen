06095db3a78d3f0165af9fd0d0cdb5a55f19fea0
Yeak Nai Siew <yeak@mailhappen.com>
2025-02-03 15:14:06 +0800

    Update mta mynetworks

--- custom/11_mta-mynetworks.sh
+++ custom/11_mta-mynetworks.sh
@@ -8,10 +8,6 @@ cd $(dirname $0)
 
 # Write your script below
 
-# CAUTION: Changing mynetworks often cause MTA down!
-# You may need to `zmcontrol restart` to fix it back
-# Always watch with zmcontrol status
-
 # Put extra network or host into this list
 # Keep this to minimal. Try get sender to do SMTP AUTH instead.
 
@@ -22,13 +18,17 @@ mynetworks="127.0.0.0/8 [::1]/128 $(hostname -i)/32"
 [ -n "$EXTRA_MYNETWORKS" ] && mynetworks="$mynetworks $EXTRA_MYNETWORKS"
 
 # check first before making changes
-current=$(su - zimbra -c 'postconf -h mynetworks')
-if [ "$current" != "$mynetworks" ]; then
-  zmhostname=$(su - zimbra -c zmhostname)
-  su - zimbra -c "zmprov ms $zmhostname zimbraMtaMyNetworks \"$mynetworks\""
-fi
-
 # NOTE about Bash variable expansion
 # OK:  su - zimbra -c 'zmprov gs `zmhostname`'
 # NOK: su - zimbra -c "zmprov gs `zmhostname`"
+
+current=$(su - zimbra -c 'postconf -h mynetworks')
+if [ "$current" != "$mynetworks" ]; then
+  zmhostname=$(su - zimbra -c zmhostname)
+  su - zimbra -c 'zmconfigdctl stop'
+  su - zimbra -c "zmprov ms $zmhostname zimbraMtaMyNetworks \"$mynetworks\""
+  su - zimbra -c 'postfix reload'
+  su - zimbra -c 'zmamavisdctl reload'
+  su - zimbra -c 'zmconfigdctl start'
+fi
 
