416644d43ecf74c96004cee25c010885109db177
Yeak Nai Siew <yeak@mailhappen.com>
2025-02-03 15:32:19 +0800

    Typo

--- custom/02_zimlet-undosend.sh
+++ custom/02_zimlet-undosend.sh
@@ -8,7 +8,7 @@ cd $(dirname $0)
 
 # Write your script below
 
-# Deploy maldua 2fa
+# Deploy undosend zimlet
 cd /tmp
 curl --max-time 30 -L https://gallery.zetalliance.org/extend/items/download/92 -o com_zimbra_undosend.zip
 su - zimbra -c 'zmzimletctl deploy /tmp/com_zimbra_undosend.zip'
