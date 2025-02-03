22ad253d2b9a3b3a809a72a0944f478cc18dc8ea
Yeak Nai Siew <yeak@mailhappen.com>
2025-02-03 18:02:33 +0800

    Improve maldun install script

--- custom/02_zimlet-maldua-2fa.sh
+++ custom/02_zimlet-maldua-2fa.sh
@@ -10,15 +10,24 @@ cd $(dirname $0)
 
 # Deploy maldua 2fa
 cd /tmp
-curl --max-time 30 -LO https://github.com/maldua-suite/zimbra-ose-2fa/releases/download/v0.8.0/zimbra-ose-2fa_0.8.0.tar.gz
-tar xf zimbra-ose-2fa_0.8.0.tar.gz
-cd zimbra-ose-2fa_0.8.0
-./install.sh
+if grep -E 'TwoFactor_qr.js' /opt/zimbra/jetty/webapps/zimbra/public/TwoFactorSetup.jsp > /dev/null 2>&1 ; then
+   :
+else
+    curl --max-time 30 -LO https://github.com/maldua-suite/zimbra-ose-2fa/releases/download/v0.8.0/zimbra-ose-2fa_0.8.0.tar.gz
+    tar xf zimbra-ose-2fa_0.8.0.tar.gz
+    cd zimbra-ose-2fa_0.8.0
+    ./install.sh
+    su - zimbra -c 'zmmailboxdctl restart'
+fi
 
 # Temp fix errorMessage showing
-cat <<EOT >> /opt/zimbra/jetty/webapps/zimbra/skins/_base/base3/skin.css
+if grep -E '.twoFactorForm .errorMessage{' /opt/zimbra/jetty/webapps/zimbra/skins/_base/base3/skin.css > /dev/null 2>&1 ; then
+    :
+else
+    cat <<EOT >> /opt/zimbra/jetty/webapps/zimbra/skins/_base/base3/skin.css
 .twoFactorForm .errorMessage{
         display: none;
 }
 EOT
-su - zimbra -c 'zmmailboxdctl restart'
+    su - zimbra -c 'zmprov fc skin'
+fi
