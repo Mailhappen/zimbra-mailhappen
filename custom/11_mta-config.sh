ffa2200f9782977e9801116a3f1f0ccdef94321a
Yeak Nai Siew <yeak@mailhappen.com>
2025-02-03 11:29:38 +0800

    More custom scripts

--- custom/11_mta-config.sh
+++ custom/11_mta-config.sh
@@ -8,33 +8,35 @@ cd $(dirname $0)
 
 # Write your script below
 
-# header modification
+# Header modification
+# capture Subject: into the log file
+# hide all localhost routes
 if [ ! -f /opt/zimbra/conf/custom_header_checks ]; then
   cat <<EOT > /opt/zimbra/conf/custom_header_checks
 /^Subject:/                 WARN
 /^Received: .*localhost.*/  IGNORE
 EOT
-  su - zimbra -c 'zmprov ms `zmhostname` zimbraMtaHeaderChecks "pcre:/opt/zimbra/conf/postfix_header_checks pcre:/opt/zimbra/conf/custom_header_checks"'
 fi
+su - zimbra -c 'zmprov ms `zmhostname` zimbraMtaHeaderChecks "pcre:/opt/zimbra/conf/postfix_header_checks pcre:/opt/zimbra/conf/custom_header_checks"'
 
-echo "Sender Restrictions"
+# Sender Restrictions
 if [ ! -f /opt/zimbra/conf/postfix_reject_sender -o ! -f /opt/zimbra/conf/slm-exceptions-db ]; then
   su - zimbra -c 'touch /opt/zimbra/conf/postfix_reject_sender'
-  su - zimbra -c 'postmap /opt/zimbra/conf/postfix_reject_sender'
   su - zimbra -c 'touch /opt/zimbra/conf/slm-exceptions-db'
+  su - zimbra -c 'postmap /opt/zimbra/conf/postfix_reject_sender'
   su - zimbra -c 'postmap /opt/zimbra/conf/slm-exceptions-db'
-  su - zimbra -c 'zmprov ms `zmhostname` zimbraMtaSmtpdSenderLoginMaps "lmdb:/opt/zimbra/conf/slm-exceptions-db, proxy:ldap:/opt/zimbra/conf/ldap-slm.cf"'
-  su - zimbra -c 'zmprov ms `zmhostname` zimbraMtaSmtpdSenderRestrictions "reject_authenticated_sender_login_mismatch check_sender_access lmdb:/opt/zimbra/conf/postfix_reject_sender"'
 fi
+su - zimbra -c 'zmprov ms `zmhostname` zimbraMtaSmtpdSenderLoginMaps "lmdb:/opt/zimbra/conf/slm-exceptions-db, proxy:ldap:/opt/zimbra/conf/ldap-slm.cf"'
+su - zimbra -c 'zmprov ms `zmhostname` zimbraMtaSmtpdSenderRestrictions "reject_authenticated_sender_login_mismatch check_sender_access lmdb:/opt/zimbra/conf/postfix_reject_sender"'
 
-echo "Manual postfix transport"
+# Add our postfix transport
 if [ ! -f /opt/zimbra/conf/postfix_transport ]; then
   su - zimbra -c 'touch /opt/zimbra/conf/postfix_transport'
   su - zimbra -c 'postmap /opt/zimbra/conf/postfix_transport'
-  su - zimbra -c 'zmprov ms `zmhostname` zimbraMtaTransportMaps "lmdb:/opt/zimbra/conf/postfix_transport, proxy:ldap:/opt/zimbra/conf/ldap-transport.cf"'
 fi
+su - zimbra -c 'zmprov ms `zmhostname` zimbraMtaTransportMaps "lmdb:/opt/zimbra/conf/postfix_transport, proxy:ldap:/opt/zimbra/conf/ldap-transport.cf"'
 
-echo "Change Lmtp Host Lookup to native"
+# Use native Lmtp Host Lookup
 cur=$(su - zimbra -c 'postconf -h lmtp_host_lookup' )
 if [ "$cur" != "native" ]; then
   su - zimbra -c 'zmprov ms `zmhostname` zimbraMtaLmtpHostLookup native'
---
+++ custom/11_mta-mynetworks.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# set -e Exit immediately if any command failed
+# set -x Enable debugging
+set -ex
+
+# Must run in the dirname of the script
+cd $(dirname $0)
+
+# Write your script below
+
+# CAUTION: Changing mynetworks often cause MTA down!
+# You may need to `zmcontrol restart` to fix it back
+# Always watch with zmcontrol status
+
+# Put extra network or host into this list
+# Keep this to minimal. Try get sender to do SMTP AUTH instead.
+
+EXTRA_MYNETWORKS=""
+
+echo "Configure mynetworks"
+mynetworks="127.0.0.0/8 [::1]/128 $(hostname -i)/32"
+[ -n "$EXTRA_MYNETWORKS" ] && mynetworks="$mynetworks $EXTRA_MYNETWORKS"
+
+# check first before making changes
+current=$(su - zimbra -c 'postconf -h mynetworks')
+if [ "$current" != "$mynetworks" ]; then
+  zmhostname=$(su - zimbra -c zmhostname)
+  su - zimbra -c "zmprov ms $zmhostname zimbraMtaMyNetworks \"$mynetworks\""
+fi
+
+# NOTE about Bash variable expansion
+# OK:  su - zimbra -c 'zmprov gs `zmhostname`'
+# NOK: su - zimbra -c "zmprov gs `zmhostname`"
+
