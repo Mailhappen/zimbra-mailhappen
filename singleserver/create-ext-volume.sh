c4cb3d214e5480c151fba8f3b163d9a009a0e518
Yeak Nai Siew <yeak@mailhappen.com>
2025-05-01 12:18:24 +0800

    Typo

--- singleserver/create-ext-volume.sh
+++ singleserver/create-ext-volume.sh
@@ -3,7 +3,7 @@
 # To ensure data safety, create our own volume and use it externally.
 # Then we store paths in a single volume for easy handling.
 
-my_volume_name=="my_optzimbra"
+my_volume_name="my_optzimbra"
 docker volume create $my_volume_name
 docker run --rm \
 	--mount src=$my_volume_name,dst=/mnt \
