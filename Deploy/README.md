11ef96447a73cd9ed043a57ddc1684b75ba7b247
Yeak Nai Siew <yeak@mailhappen.com>
2025-09-19 12:36:17 +0800

    Renaming files

--- Deploy/README.md
+++ Deploy/README.md
@@ -2,4 +2,5 @@
 
 This place is for PS team to deploy Zimbra.
 
+Refer to [QUICKSTART](../QUICKSTART.md) for deployment.
 
--- Deploy/custom-sample/00_template.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# set -x Enable debugging
-set -x
-
-# Must run in the dirname of the script
-cd $(dirname $0)
-
-# Write your script below
